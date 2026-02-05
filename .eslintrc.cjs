50a86de1be58f7721292b5abb5babbed420e1f32
Ryan Palermo <ryan@ryanpalermo.dev>
2026-02-04 23:07:48 -0500

    chore: scaffold tooling config

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  ignorePatterns: ["dist", "node_modules"]
+};
